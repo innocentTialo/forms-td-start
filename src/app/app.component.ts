998254f985c28016cee8535eba6103aff58e9c49
InnocentTIALO <tialoinnocent@gmail.com>
2020-02-04 04:12:34 +0100

    learn how to properly define default value of fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f', {static: false}) signupForm: NgForm;
+  secretQuestion = 'teacher';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }

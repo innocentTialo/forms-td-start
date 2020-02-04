953e1c34d7165689be15bdaa6f65e0020c5990ef
InnocentTIALO <tialoinnocent@gmail.com>
2020-02-04 05:19:42 +0100

    learn how to collect data when submitting form and how to reset form to a specific values or just reset it to empty value lik page had been reloaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,30 @@ export class AppComponent {
   secretQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /*this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      answer: '',
+      gender: 'male'
+    });*/
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   /*onSubmit(form: NgForm) {
@@ -22,6 +43,20 @@ export class AppComponent {
   }*/
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset({
+      userData: {
+        username: '',
+        email: ''
+      },
+      secret: 'pet',
+      answer: '',
+      gender: 'male'
+    });
   }
 }
